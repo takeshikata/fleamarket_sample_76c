1e7d9780a8f9ff19328ab48e321f581d536aff6f
yamaton2004js <b07.lucky.boy26@docomo.ne.jp>
2020-06-05 16:55:55 +0900

    create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.create(product_params)
   end
 
   def edit
@@ -16,5 +17,10 @@ class ProductsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :introduction, :price)
   end
 end
