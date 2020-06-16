9e070d0e733e7d3068ec91b872a5299ec326d6c4
takeshikata <kataoka1053559@gmail.com>
2020-06-16 16:21:03 +0900

    evaluation-sum

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,17 @@ class UsersController < ApplicationController
 
   def show
     @product = Product.find_by(user_id: params[:id])
+    @products = Product.where(user_id: params[:id])
 
     if @product
       d_evaluations = Evaluation.select(:user_id, :product_id, :evaluation).distinct
-      @evaluation_good_count = d_evaluations.where(evaluation: :good, product_id: @product.id).where.not(user_id: @product.user_id).count
-      @evaluation_normal_count = d_evaluations.where(evaluation: :normal, product_id: @product.id).where.not(user_id: @product.user_id).count
-      @evaluation_bad_count = d_evaluations.where(evaluation: :bad, product_id: @product.id).where.not(user_id: @product.user_id).count
+
+      @evaluation_good_sum, @evaluation_normal_sum, @evaluation_bad_sum = 0, 0, 0
+      @products.each do |product|
+        @evaluation_good_sum += d_evaluations.where(evaluation: :good, product_id: product.id).where.not(user_id: product.user_id).count
+        @evaluation_normal_sum += d_evaluations.where(evaluation: :normal, product_id: product.id).where.not(user_id: product.user_id).count
+        @evaluation_bad_sum += d_evaluations.where(evaluation: :bad, product_id: product.id).where.not(user_id: product.user_id).count
+      end
     end
   end
 
