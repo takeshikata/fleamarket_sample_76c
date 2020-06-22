3a330b2c1de7cd5708d9adb535427375a23d8495
Saw385 <63223220+Saw385@users.noreply.github.com>
2020-06-22 10:19:43 +0900

    Add_change

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,10 +5,12 @@ class CardsController < ApplicationController
   before_action :set_card
 
   def new
+    @user = User.find(current_user.id)
     card = Card.where(user_id: current_user.id)
     redirect_to card_path(current_user.id) if card.exists?
+    @profile = Profile.find(current_user.id)
   end
-
+  
   def create
     #payjpとCardのデータベース作成
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
@@ -20,31 +22,33 @@ class CardsController < ApplicationController
         description: 'test',
         card: params['payjp-token'],
         metadata: {user_id: current_user.id}
-      )
-      @card = Card.new(user_id: current_user.id, payjp_id: customer.id)
-      if @card.save
-        redirect_to card_path(current_user.id)
-      else
-        redirect_to new_card_path
+        )
+        @card = Card.new(user_id: current_user.id, payjp_id: customer.id)
+        if @card.save
+          redirect_to card_path(current_user.id)
+        else
+          redirect_to new_card_path
+        end
       end
     end
-  end
-
-  def destroy
-    #PayjpとCardデータベースを削除
-    card = Card.find_by(user_id: current_user.id)
-    if card.blank?
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(@card.payjp_id)
-      customer.delete
-      card.delete
-    end
+    
+    def destroy
+      #PayjpとCardデータベースを削除
+      card = Card.find_by(user_id: current_user.id)
+      if card.blank?
+      else
+        Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+        customer = Payjp::Customer.retrieve(@card.payjp_id)
+        customer.delete
+        card.delete
+      end
       redirect_to new_card_path
-  end
-
-  def show
-    #Cardのデータpayjpに送り情報を取り出す
+    end
+    
+    def show
+      #Cardのデータpayjpに送り情報を取り出す
+    @user = User.find(current_user.id)
+    @profile = Profile.find(current_user.id)
     card = Card.find_by(user_id: current_user.id)
     if card.blank?
       redirect_to new_card_path 
