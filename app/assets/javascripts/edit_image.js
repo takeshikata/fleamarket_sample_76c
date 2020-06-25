b8c5b37deeb9c9610e0e9f390b44f9564de37478
takeshikata <kataoka1053559@gmail.com>
2020-06-25 15:49:16 +0900

    error-handling

--- app/assets/javascripts/edit_image.js
+++ app/assets/javascripts/edit_image.js
@@ -12,7 +12,34 @@ $(function () {
       })
     }
   }
+  if (window.location.href.match(/\/products\/[1-9]?[0-9]\/edit/)){
+    $(".product-new-form").on("submit", function(){
+      console.log("submit");
+      var name_count = $(".input-form-xregular").val().length;
+      var introduction_count = $(".input-form-xlarge").val().length;
+      var price_count = $(".input-form-middle").val().length;
+      console.log(name_count);
+      if ( !name_count ){
+        console.log("name");
+        alert("商品名を入れてください");
+        // $('.xsubmit__btn').prop('disabled', false);
+        return false;
+      }
+      else if ( !introduction_count ){
+        console.log("introduction");
+        alert("商品説明欄をご記入ください");
+        return false;
+      }
+      else if ( !price_count ){
+        console.log("price");
+        alert("価格を入れてください");
+        return false;
+      }
+      else if ($(`#preview-box__0`).length == 0){
+        console.log("画像が0個のとき");
+        alert("画像を添付してください");
+        return false;
+      }
+    });
+  }
 });
-
-
-
